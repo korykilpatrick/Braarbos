f107dae69b0c27132581c8839f8fd2727605c0be
Kory <koryrkilpatrick@gmail.com>
2017-12-13 12:04:02 -0800

    accessing cryptonator api

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 
 class Homepage extends React.Component {
   constructor() {
     super();
     this.state = {
-      user: 'Jeff'
+      user: 'Jeff',
+      markets: []
     }
+    this.getPrices = this.getPrices.bind(this);
+  }
+
+  componentDidMount() {
+    this.getPrices()
+  }
+
+  getPrices() {
+    axios.get('/markets')
+      .then(markets => this.setState({ markets: markets.data }))
+      .catch(err => console.log(err))
   }
 
   render() {
     return (
       <div>
-        {this.state.user}
+        Welcome to Braarbos, {this.state.user}, lets make some fucking coin
+        <div>
+        The current price of Bitcoin is 
+        {this.state.markets.map(market => {
+          return <li>${Math.floor(market.price)} on {market.market}</li>})}
+        </div>
       </div>)
   }
 }
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const axios = require('axios');
 require('dotenv').config();
 
 const app = express();
@@ -7,5 +8,12 @@ const port = process.env.PORT;
 const server = http.createServer(app);
 
 server.listen(port, () => console.log(`paper getting stacked at port ${port}`));
-
 app.use(express.static(`${__dirname}./../client`));
+
+app.get('/markets', (req, res) => {
+  axios.get('https://api.cryptonator.com/api/full/btc-usd')
+   .then(results => res.json(results.data.ticker.markets))
+   .catch(err => console.error(err))
+});
+
+
